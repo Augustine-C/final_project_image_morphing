23c2092d67924df669bdb33b20be23bcb016c12c
Zhengxi Li <lzx404243@hotmail.com>
2021-12-07 17:48:00 -0600

    added naive-cross-dissolve

--- utils.py
+++ utils.py
@@ -45,3 +45,40 @@ def get_mask(triangle, img):
 #     plt.imshow(np.uint8(mask), cmap='gray')
     return mask
 
+
+def imageFolder2mpeg(input_path, output_path='./output_video.mpeg', fps=30.0):
+    '''
+    Extracts the frames from an input video file
+    and saves them as separate frames in an output directory.
+    Adapted from CS445-MP5
+    Input:
+        input_path: Input video file.
+        output_path: Output directorys.
+        fps: frames per second (default: 30).
+    Output:
+        None
+    '''
+
+    dir_frames = input_path
+    files_info = os.scandir(dir_frames)
+
+    file_names = [f.path for f in files_info if f.name.endswith(".jpg")]
+    file_names.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
+
+    frame_Height, frame_Width = cv2.imread(file_names[0]).shape[:2]
+    resolution = (frame_Width, frame_Height)
+
+    # Define the codec and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*'MPG1')
+    video_writer = cv2.VideoWriter(output_path, fourcc, fps, resolution)
+
+    frame_count = len(file_names)
+
+    frame_idx = 0
+
+    while frame_idx < frame_count:
+        frame_i = cv2.imread(file_names[frame_idx])
+        video_writer.write(frame_i)
+        frame_idx += 1
+
+    video_writer.release()
